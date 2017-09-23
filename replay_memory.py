3604faf6b64d925a7fa89854a4baf4ae8616ffc1
bakanaouji <abeken1224@yahoo.co.jp>
2017-09-23 22:10:06 +0900

    replay_memory.py追加

--- replay_memory.py
+++ replay_memory.py
@@ -1,4 +1,8 @@
-class ReplayMemory():
+import numpy as np
+import random
+from collections import deque
+
+class ReplayMemory(object):
     def __init__(self, size):
         self._deque = deque()
         self._max_size = size
@@ -12,3 +16,13 @@ class ReplayMemory():
         # Replay Memoryが一定数を超えたら，古い遷移から削除
         if len(self._deque) > self._max_size:
             self._deque.popleft()
+
+    def sample(self, minibatch_size):
+        # Replay Memoryからランダムにミニバッチをサンプル
+        # @todo 非復元か復元か確認
+        idxes = [np.random.randint(0, len(self._deque)) for _ in range(minibatch_size)]
+        data = [self._deque[i] for i in idxes]
+        return data
+
+    def __getitem__(self, index):
+        return self._deque[index]
