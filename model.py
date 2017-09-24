9ac2e048d3019fb0599d21dca182b6e59913ecf7
bakanaouji <abeken1224@yahoo.co.jp>
2017-09-24 22:04:32 +0900

    model.py追加

---
+++ model.py
@@ -0,0 +1,29 @@
+from keras.layers import Conv2D, Flatten, Dense
+from keras.models import Sequential
+
+def init_q_network(num_actions, agent_history_length, frame_width, frame_height):
+    """
+    Q-Networkをランダムな重みで初期化
+    :param num_actions:　行動数
+    :return:
+        s: Q-Networkへの入力
+        q_values: Q-Networkの出力
+        model: Q-Networkのモデル
+    """
+    model = Sequential()
+    model.add(Conv2D(32, (8, 8), strides=(4, 4), activation='relu',
+        input_shape=(
+            agent_history_length, frame_width,
+            frame_height, data_format='channels_first'
+            )
+        )
+    model.add(Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(num_actions))
+    s = tf.placeholder(
+        tf.float32,[None, agent_history_length, frame_width, frame_height]
+        )
+    q_values = model(s)
+
+    return s, q_values, model
