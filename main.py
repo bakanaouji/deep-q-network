18435b642c619de65717fd3d760f9fd3c35072b0
bakanaouji <abeken1224@yahoo.co.jp>
2017-09-30 00:41:38 +0900

    DQNクラスとりあえず動くところまで

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ def main():
     env = gym.make("PongNoFrameskip-v4")
     env = wrap_dqn(env)
     params = {}
+
+    # 環境側のパラメータ
+    params['frame_width'] = 84  # リサイズ後のフレーム幅
+    params['frame_height'] = 84  # リサイズ後のフレーム高さ
+    params['agent_history_length'] = 4  # Q_networkの入力として与える，直近のフレームの数
+    params['replay_start_size'] = 10  # 学習を始める前に，このフレーム数に対して一様ランダムに行動を選択する政策が実行され，その経験がReplay memoryに蓄えられる
+
+    # DQNのアルゴリズムのパラメータ
     params['learning_rate'] = 0.00025  # RMSPropで使用される学習率
+    params['discount_factor'] = 0.99  # Q_learningの更新で用いられる割引率γ
     params['gradient_momentum'] = 0.95  # RMSPropで使用されるGradient momuntum
     params['squared_gradient_momuntum'] = 0.95  # RMSPropで使用されるSquared gradient (denominator) momentum
     params['min_squared_gradient'] = 0.01  # RMSPropの更新の際，Squared gradient (denominator)に加算される定数
@@ -15,10 +24,11 @@ def main():
     params['final_exploration'] = 0.1  # ε-greedyにおけるεの最終値
     params['final_exploration_frame'] = 1000000  # εが初期値から最終値に線形減少した時に，最終値に到達するフレーム数
     params['replay_memory_size'] = 500000  # SGDによる更新に用いるデータは，このサイズの直近のフレームデータからサンプルする
-    params['frame_width'] = 84  # リサイズ後のフレーム幅
-    params['frame_height'] = 84  # リサイズ後のフレーム高さ
-    params['agent_history_length'] = 4  # Q_networkの入力として与える，直近のフレームの数
+    params['minibatch_size'] = 32  # SGDによる更新に用いる訓練データの数
+    params['target_network_update_frequency'] = 10000  # target_networkが更新される頻度（パラメータの更新頻度数で計測）
+
     params['tmax'] = 50000000  # 学習をやめる行動数
+
     trainer = Trainer(env, **params)
     # 学習実行
     trainer.train()
--- trainer.py
+++ trainer.py
@@ -7,7 +7,13 @@ from model import init_q_network
 class Trainer(object):
     def __init__(self, env, **params):
         self.env = env
+        self.frame_width = params['frame_width']
+        self.frame_height = params['frame_height']
+        self.agent_history_length = params['agent_history_length']
+        self.replay_start_size = params['replay_start_size']
+
         self.learning_rate = params['learning_rate']
+        self.discount_factor = params['discount_factor']
         self.gradient_momentum = params['gradient_momentum']
         self.squared_gradient_momuntum = params['squared_gradient_momuntum']
         self.min_squared_gradient = params['min_squared_gradient']
@@ -15,9 +21,10 @@ class Trainer(object):
         self.final_exploration = params['final_exploration']
         self.final_exploration_frame = params['final_exploration_frame']
         self.replay_memory_size = params['replay_memory_size']
-        self.frame_width = params['frame_width']
-        self.frame_height = params['frame_height']
-        self.agent_history_length = params['agent_history_length']
+        self.minibatch_size = params['minibatch_size']
+        self.target_network_update_frequency = params[
+            'target_network_update_frequency']
+
         self.tmax = params['tmax']
         
     def build_training_op(self, num_actions, q_values, q_network_weights):
@@ -49,6 +56,42 @@ class Trainer(object):
             action = np.argmax(q_values.eval(
                 feed_dict={s: [observation]}))
         return action
+
+    def learn(self, sess, s, a, y, loss, grad_update, replay_memory, target_q_values, target_s):
+        state_batch = []
+        action_batch = []
+        reward_batch = []
+        next_state_batch = []
+        done_batch = []
+        # Replay Memoryからランダムにミニバッチをサンプル
+        minibatch = replay_memory.sample(self.minibatch_size)
+
+        for data in minibatch:
+            state_batch.append(data[0])
+            action_batch.append(data[1])
+            reward_batch.append(data[2])
+            next_state_batch.append(data[3])
+            done_batch.append(data[4])
+
+        # 終了判定を，1（True），0（False）に変換
+        done_batch = np.array(done_batch) + 0
+
+        # Target Networkで次の状態でのQ値を計算
+        target_q_values_batch = target_q_values.eval(
+            feed_dict={target_s: np.float32(np.array(
+                next_state_batch))})
+        # 教師信号を計算
+        y_batch = reward_batch + (
+            1.0 - done_batch) * self.discount_factor * np.max(
+            target_q_values_batch,
+            axis=1)
+        # 勾配法による誤差最小化
+        l, _ = sess.run([loss, grad_update], feed_dict={
+            s: np.float32(np.array(state_batch)),
+            a: action_batch,
+            y: y_batch
+        })
+        return l
 
     def train(self):
         # Replay Memory
@@ -96,9 +139,28 @@ class Trainer(object):
             observation = self.env.reset()
             # エピソード終了まで実行
             while not done:
+                # 前の状態を保存
+                pre_observation = observation.copy()
                 # ε-greedyに従って行動を選択
                 action = self.choose_action_by_epsilon_greedy(self.env.action_space.n, s, q_values, epsilon, observation)
+                # 行動を実行し，報酬と次の画面とdoneを観測
+                observation, reward, done, info = self.env.step(action)
+                # replay memoryに(s_t,a_t,r_t,s_{t+1},done)を追加
+                # deepcopyになってるかな？？
+                replay_memory.add(pre_observation, action, reward, observation, done)
+                self.env.render()
                 # εを線形減少
                 epsilon = max(self.final_exploration,
                                     epsilon - (
                                     self.initial_exploration - self.final_exploration) / self.final_exploration_frame)
+                if t > self.replay_start_size:
+                    # Q-Networkの学習
+                    total_loss += self.learn(sess, s, a, y, loss, grad_update, replay_memory, target_q_values, target_s)
+                if t % (self.target_network_update_frequency) == 0:
+                    # Target Networkの更新
+                    sess.run(assign_target_network)
+                total_reward += reward
+                total_q_max += np.max(q_values.eval(
+                    feed_dict={s: [observation]}))
+                t += 1
+                duration += 1
