9dae2eb7f09875c734325b0b54f7e863525c933c
bakanaouji <abeken1224@yahoo.co.jp>
2017-09-30 02:52:21 +0900

    リファクタリング中

--- replay_memory.py
+++ replay_memory.py
@@ -4,8 +4,7 @@ from collections import deque
 
 class ReplayMemory(object):
     def __init__(self, size):
-        self._deque = deque()
-        self._max_size = size
+        self._deque = deque(maxlen=size)
 
     def __len__(self):
         return len(self._deque)
@@ -13,9 +12,6 @@ class ReplayMemory(object):
     def add(self, state, action, reward, next_state, done):
         # Replay Memoryに遷移を保存
         self._deque.append((state, action, reward, next_state, done))
-        # Replay Memoryが一定数を超えたら，古い遷移から削除
-        if len(self._deque) > self._max_size:
-            self._deque.popleft()
 
     def sample(self, minibatch_size):
         # Replay Memoryからランダムにミニバッチをサンプル
